b01f1f4dbbe4c064547a86ba1d406d6fa7e35f2c
yile yang <375911733@qq.com>
2024-09-04 16:45:09 +0700

    使用IDEA创建空项目

---
+++ src/main/java/com/example/demojavabtcsign/DemojavabtcsignApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavabtcsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavabtcsignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojavabtcsignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojavabtcsign/DemojavabtcsignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojavabtcsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojavabtcsignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
